dea83e91f7f9929b7079678ef5266c5fc9349298
OmmU06 <89683174+OmmU06@users.noreply.github.com>
2022-06-08 21:58:56 +0530

    Add files via upload

---
+++ WEBSITE/js/index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: twitter, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'twitter.json' // the path to the animation json
+  });
